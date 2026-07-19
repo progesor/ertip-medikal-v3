7f3d75ce17d4252d7caac13414bfc17ba43b6568
Catlak Profesor <progesor@gmail.com>
2026-07-19 12:09:08 +0300

    sss

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,7 @@ import { withPayload } from "@payloadcms/next/withPayload";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        reactCompiler: false, // Next 15 RC ve Payload Beta uyumu için şimdilik kapalı
-    },
+    reactCompiler: false,
 };
 
 export default withPayload(nextConfig);
