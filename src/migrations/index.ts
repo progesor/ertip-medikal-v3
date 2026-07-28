11373b87da9f85c06c188504ef6f5ecbf012cb9b
Catlak Profesor <progesor@gmail.com>
2026-07-28 10:43:09 +0300

    feat(database): register configurable SKU migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260727_101107_production_baseline from './20260727_101107_production_baseline';
 import * as migration_20260727_131359_image_optimization_settings from './20260727_131359_image_optimization_settings';
+import * as migration_20260728_101500_configurable_sku_rules from './20260728_101500_configurable_sku_rules';
 
 export const migrations = [
   {
@@ -11,5 +12,10 @@ export const migrations = [
     up: migration_20260727_131359_image_optimization_settings.up,
     down: migration_20260727_131359_image_optimization_settings.down,
     name: '20260727_131359_image_optimization_settings'
+  },
+  {
+    up: migration_20260728_101500_configurable_sku_rules.up,
+    down: migration_20260728_101500_configurable_sku_rules.down,
+    name: '20260728_101500_configurable_sku_rules'
   },
 ];
