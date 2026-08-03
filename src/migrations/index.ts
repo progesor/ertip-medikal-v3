917f280a37075c7952365bb255607dd175b8089e
Catlak Profesor <progesor@gmail.com>
2026-08-03 08:45:22 +0300

    chore(migrations): register access-code redaction

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -6,6 +6,7 @@ import * as migration_20260730_053858_media_text_layouts from './20260730_053858
 import * as migration_20260731_075754_m9_page_builder_foundations from './20260731_075754_m9_page_builder_foundations';
 import * as migration_20260731_101005_m9_content_block_polish from './20260731_101005_m9_content_block_polish';
 import * as migration_20260731_113512_m9_new_content_blocks from './20260731_113512_m9_new_content_blocks';
+import * as migration_20260803_060000_redact_download_access_codes from './20260803_060000_redact_download_access_codes';
 
 export const migrations = [
   {
@@ -46,6 +47,11 @@ export const migrations = [
   {
     up: migration_20260731_113512_m9_new_content_blocks.up,
     down: migration_20260731_113512_m9_new_content_blocks.down,
-    name: '20260731_113512_m9_new_content_blocks'
+    name: '20260731_113512_m9_new_content_blocks',
+  },
+  {
+    up: migration_20260803_060000_redact_download_access_codes.up,
+    down: migration_20260803_060000_redact_download_access_codes.down,
+    name: '20260803_060000_redact_download_access_codes',
   },
 ];
